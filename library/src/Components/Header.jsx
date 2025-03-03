b683e7e80b7489a8ac6774465d854fea20156144
divyanshuraj1095 <divyanshuraj23102005@gmail.com>
2025-03-03 11:29:20 +0530

    third commit

--- library/src/Components/Header.jsx
+++ library/src/Components/Header.jsx
@@ -1,11 +1,19 @@
 import soeicon from '../assets/soe icon.jpeg';
 import { useEffect, useState } from 'react';
-
+import { GiHamburgerMenu } from "react-icons/gi";
 import usericon from '../assets/usericon.svg';
 import Button from '../Components/Button.jsx';
+import CatPapers from './HeaderButton/CatPapers.jsx'
 
 const Header = ({ setAbout,setMyBooks}) => {
-
+    const [resources, setResources] = useState("")
+    const [others, setOthers]=useState("")
+    const handleDropbar=(event)=>{
+        setResources(event.target.value)
+    }
+    const handleOthers =(event)=>{
+        setOthers(event.target.value)
+    }
 
     return (
         <>
@@ -15,6 +23,7 @@ const Header = ({ setAbout,setMyBooks}) => {
                         <img src={soeicon} alt='college icon' />
                         <h2>SOE Library</h2>
                     </div>
+                    <GiHamburgerMenu className='vegburger'/>
 
                     <nav className='button'>
                      <Button label="Home" onClick={() => {
@@ -26,9 +35,24 @@ const Header = ({ setAbout,setMyBooks}) => {
                      <Button label="About" onClick={() => {
                          setAbout(true); 
                          setMyBooks(false); }} />
-                     <Button label=""/>
+                    
                     </nav>
                 </nav>
+                <select value={resources} onClick={handleDropbar}className='resources'>
+                        <option>Resources</option>
+                        <option value="catPapers">Cat Papers</option>
+                        <option value="institutionalRepo">Institutional Repository</option>
+                        <option value="eNewspaper">E-Newspaper</option>
+                        <option value="eBooks">E-Books</option>
+                        <option value="indianJournals">Indian Journals</option>
+                     </select>
+
+                <select value={others} onclick={handleOthers} className='resources'>
+                    <option>Others</option>
+                    <option value='tutorials'>Tutotrials</option>
+                    <option value='gallary'>Gallary</option>
+                    <option value='contact'>Contact</option>
+                    </select>     
 
                 
 
---
+++ library/src/Components/HeaderButton/CatPapers.jsx
@@ -0,0 +1,6 @@
+const CatPapers = ()=>{
+    return(<>
+    
+    </>)
+}
+export default CatPapers
